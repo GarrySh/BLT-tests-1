0f847783b38a95920d837985c3e520262a110b7f
Garry Sh <sooo.lame@gmail.com>
2019-05-11 23:34:44 +1000

    add solution

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+/* eslint-disable no-alert */
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const firstNumber = document.getElementById('firstNumber');
+const secondNumber = document.getElementById('secondNumber');
+
+const form = document.getElementById('calcForm');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const number1 = Number(firstNumber.value);
+  const number2 = Number(secondNumber.value);
+  const sum = number1 + number2;
+  const result = Number(sum.toFixed(10));
+  if (Number.isNaN(result)) {
+    alert('Разрешается вводить только числа');
+  } else {
+    alert(`Результат: ${result}`);
+  }
+});
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -24,6 +24,10 @@ export default webpackEnv => {
           exclude: /node_modules/,
           use: 'babel-loader',
         },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
       ],
     },
     plugins: [new HtmlWebpackPlugin({ template: 'public/index.html' })],
